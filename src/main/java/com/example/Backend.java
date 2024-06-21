d8e4b4a4b33599392057f0ff929d287e9f12ede9
Raphael Tholl <raphael.tholl@ibm.com>
2024-06-21 15:31:45 +0200

    basic echo POST API

--- src/main/java/com/example/Backend.java
+++ src/main/java/com/example/Backend.java
@@ -1,16 +1,24 @@
 package com.example;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/")
+public class Backend {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @POST
+    @Path("echo")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String echo(String message) {
+        return message;
     }
 }
